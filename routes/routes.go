1fd086ec18bdbb175e7b9f9acb059f2cbf1a0370
farnoodlotfali <farnoodlotfali@gmail.com>
2025-05-29 17:26:04 +0330

    add new routes

--- routes/routes.go
+++ routes/routes.go
@@ -4,9 +4,27 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 	api := server.Group("/api")
+
+	// games
 	api.GET("/games", getGames)
 	api.POST("/games", createGame)
+	api.PUT("/games/:id", updateGame)
+	api.GET("/games/:id", getGame)
 
-	api.POST("/publisher", createPublisher)
+	// publishers
+	api.POST("/publishers", createPublisher)
 
+	// genre
+	api.GET("/genres", getGenres)
+	api.POST("/genres", createGenre)
+
+	// games- genre
+	api.POST("/games/:id/genres", gameAddGenre)
+
+	// platform
+	api.GET("/platforms", getPlatforms)
+	api.POST("/platforms", createPlatform)
+
+	// games- platforms
+	api.POST("/games/:id/platforms", gameAddPlatform)
 }
